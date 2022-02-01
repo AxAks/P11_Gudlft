99fd4f2a1a0134a06318a938ad0d54ad81a7c076
ax <axel.akonde@cinego.net>
2022-02-01 10:35:21 +0100

    WIP: premiere approche et essais avec locust

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -1,14 +1,32 @@
 from locust import HttpUser, task
-import server
 
 
-class HelloWorldUser(HttpUser):
+class TestUser(HttpUser):
+
     @task
     def index(self):
-        self.client = server.app.client()
         self.client.get("/")
 
+
 """
+@task
+def show_summary(self):
+    self.client.post("/show_summary")
+
+
+@task
+def book(self):
+    self.client.get("/book/<competition_name>/<club_name>")
+
+@task
+def purchase_places(self):
+    self.client.post("/purchase_places")
+
+@task
+def logout(self):
+    self.client.get("/logout")
+
+
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
